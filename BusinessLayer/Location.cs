6dd22c3cbac82d84a1de6bd62b58c1110dd77108
MilenSh <milen_shopov@protonmail.com>
2023-10-19 20:59:09 +0300

    added butload to Location class

--- BusinessLayer/Location.cs
+++ BusinessLayer/Location.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,37 @@ namespace BusinessLayer
 {
 	public class Location
 	{
+
+        [Key]
+		public int Id { get; set; }
+
+		[Required]
+		public string? Name { get; set; }
+
 		public Coordinates Coordinates { get; set; }
-	}
+
+		public List<TrainComposition> TrainCompositions { get; set; }
+
+		public List<Locomotive> Locomotives { get; set; }
+
+		public List<TrainCar> TrainCars { get; set; }
+
+        public Location() 
+        {
+            TrainCompositions = new List<TrainComposition>();
+            Locomotives = new List<Locomotive>();
+            TrainCars = new List<TrainCar>();
+        }
+
+        public Location(string? name, Coordinates coordinates)
+        {
+            Name = name;
+            Coordinates = coordinates;
+            TrainCompositions = new List<TrainComposition>();
+            Locomotives = new List<Locomotive>();
+            TrainCars = new List<TrainCar>();
+        }
+
+
+    }
 }
