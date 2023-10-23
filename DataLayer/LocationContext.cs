74c9e5f88e09843f6a683b314e1b93799de42fe9
MilenSh <milen_shopov@protonmail.com>
2023-10-23 10:32:37 +0300

    added TrainCarContext, LocationContext

---
+++ DataLayer/LocationContext.cs
@@ -0,0 +1,135 @@
+﻿using BusinessLayer;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    public class LocationContext : IDb<Location, int>
+    {
+        private readonly TrainDbContext dbContext;
+
+        public LocationContext() 
+        { 
+            dbContext = new TrainDbContext();
+        }
+
+        public async Task CreateAsync(Location item)
+        {
+            try
+            {
+                this.dbContext.Add(item);
+                await dbContext.SaveChangesAsync();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public async Task DeleteAsync(int key)
+        {
+            try
+            {
+                Location locationFromDb = await ReadAsync(key, false, false);
+                if (locationFromDb!=null)
+                {
+                    dbContext.Locations.Remove(locationFromDb);
+                    await dbContext.SaveChangesAsync();
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public async Task<ICollection<Location>> ReadAllAsync(bool useNavigationalProperties = false, bool isReadOnly = true)
+        {
+            try
+            {
+                IQueryable<Location> query = dbContext.Locations;
+
+                if (useNavigationalProperties)
+                {
+                    query.Include(x => x.TrainCompositions)
+                        .Include(x => x.TrainCars)
+                        .Include(x => x.Locomotives);
+                }
+
+                if (isReadOnly)
+                {
+                    query = query.AsNoTrackingWithIdentityResolution();
+                }
+
+                return await query.ToListAsync();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public async Task<Location> ReadAsync(int key, bool useNavigationalProperties = false, bool isReadOnly = true)
+        {
+            try
+            {
+                IQueryable<Location> query = dbContext.Locations;
+                
+                if (useNavigationalProperties)
+                {
+                    query.Include(x => x.TrainCompositions)
+                        .Include(x => x.TrainCars)
+                        .Include(x => x.Locomotives);
+                }
+
+                if (isReadOnly)
+                {
+                    query = query.AsNoTrackingWithIdentityResolution();
+                }
+
+                return await query.FirstOrDefaultAsync(x=>x.Id == key);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public async Task UpdateAsync(Location item, bool useNavigationalProperties = false)
+        {
+            try
+            {
+                Location locationFromDb = await ReadAsync(item.Id, useNavigationalProperties, false);
+
+                if (locationFromDb == null)
+                {
+                    await CreateAsync(item);
+                    return;
+                }
+
+                dbContext.Entry(locationFromDb).CurrentValues.SetValues(item);
+
+                if (useNavigationalProperties)
+                {
+                    //location will be changed from given class
+                }
+
+                await dbContext.SaveChangesAsync();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
---
+++ DataLayer/TrainCarContext.cs
@@ -0,0 +1,137 @@
+﻿using BusinessLayer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    public class TrainCarContext : IDb<TrainCar, int>
+    {
+        private readonly TrainDbContext dbContext;
+
+        public TrainCarContext() 
+        {
+            dbContext = new TrainDbContext();    
+        }
+
+        public async Task CreateAsync(TrainCar item)
+        {
+            try
+            {
+                this.dbContext.Add(item);
+                await dbContext.SaveChangesAsync();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public async Task DeleteAsync(int key)
+        {
+            try
+            {
+                TrainCar trainCarFromDb = await ReadAsync(key, false, false);
+
+                if (trainCarFromDb != null)
+                {
+                    dbContext.TrainCars.Remove(trainCarFromDb);
+                    await dbContext.SaveChangesAsync();
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public async Task<ICollection<TrainCar>> ReadAllAsync(bool useNavigationalProperties = false, bool isReadOnly = true)
+        {
+            try
+            {
+                IQueryable<TrainCar> query = dbContext.TrainCars;
+
+                if (useNavigationalProperties)
+                {
+                    query.Include(x => x.Location).Include(x => x.TrainComposition);
+                }
+
+                if (isReadOnly)
+                {
+                    query = query.AsNoTrackingWithIdentityResolution();
+                }
+
+                return await query.ToListAsync();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public async Task<TrainCar> ReadAsync(int key, bool useNavigationalProperties = false, bool isReadOnly = true)
+        {
+            try
+            {
+                IQueryable<TrainCar> query = dbContext.TrainCars;
+
+                if (useNavigationalProperties)
+                {
+                    query.Include(x => x.Location).Include(x => x.TrainComposition);
+                }
+
+                if (isReadOnly)
+                {
+                    query = query.AsNoTrackingWithIdentityResolution();
+                }
+
+                return await query.FirstOrDefaultAsync(x => x.Id == key);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public async Task UpdateAsync(TrainCar item, bool useNavigationalProperties = false)
+        {
+            try
+            {
+                TrainCar trainCarFromDb = await ReadAsync(item.Id, useNavigationalProperties, false);
+
+                if (trainCarFromDb == null) 
+                { 
+                    await CreateAsync(item);
+                    return;
+                }
+
+                dbContext.Entry(trainCarFromDb).CurrentValues.SetValues(item);
+
+                if (useNavigationalProperties)
+                {
+                    TrainComposition trainCompositionFromDb = await dbContext.TrainCompositions.FindAsync(item.TrainCompositionId);
+
+                    if (trainCompositionFromDb != null)
+                    {
+                        item.TrainComposition = trainCompositionFromDb;
+                    }
+                }
+
+                await dbContext.SaveChangesAsync();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
